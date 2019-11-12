eba46060ce8b47d0c887edd160fc4b5d695ffd17
William Nguyen <williamnguyen@Williams-MacBook-Pro.local>
2019-11-12 12:42:32 -0800

    getAdjacentNeighbors

--- src/Maze.java
+++ src/Maze.java
@@ -18,20 +18,34 @@ public class Maze {
         adjMatrix[numVertices - 1][numVertices - 1] = 1;
     }
 
+
     /* Standard accessor methods for encapsulation */
     public int[][] getAdjMatrix() { return this.adjMatrix; }
     public int getNumVertices() { return this.numVertices; }
+
 
     /**
      *
      * @return a list of coordinate pairs in the adj-matrix
      */
     public ArrayList<Cell> findAdjacentNeighbors(Cell cell) {
+
+        // list of adjacent neighbors
         ArrayList<Cell> neighbors = new ArrayList<>();
 
-        // method body
+        // Conditionals to set valid values of starting and ending values for i and j of the loop
+        int startPosX = ((cell.getX() - 1) < 0) ? cell.getX() : cell.getX() - 1;
+        int endPosX = ((cell.getX() +  1) > (numVertices - 1)) ? cell.getX() : cell.getX() + 1;
+        int startPosY = ((cell.getY() - 1) < 0) ? cell.getY() : cell.getY() - 1;
+        int endPosY = ((cell.getY() + 1 > (numVertices - 1))) ? cell.getY() : cell.getY() + 1;
 
+        // grid[i][j] are the neighbors of the current cell
+        for(int i  = startPosX; i < endPosX; i++) {
+            for(int j = startPosY; j < endPosY; j++) {
+                // adding to a list of cells so that we can keep track of individual coordinates
+                neighbors.add(new Cell(i, j));
+            }
+        }
         return neighbors;
     }
-
 }
