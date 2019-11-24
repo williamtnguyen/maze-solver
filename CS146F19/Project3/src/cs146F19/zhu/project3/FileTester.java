ab5960b770f674114ccbfb0b7b2f6e07f8a5599d
unknown <justin.zhu@sjsu.edu>
2019-11-24 10:49:19 -0800

    Output file mostly complete, Everything printing wise complete

--- CS146F19/Project3/src/cs146F19/zhu/project3/FileTester.java
+++ CS146F19/Project3/src/cs146F19/zhu/project3/FileTester.java
@@ -12,11 +12,11 @@ public class FileTester {
 		File m8 =new File("sample-inputs/maze8.txt");
 		File m10 =new File("sample-inputs/maze10.txt");
 		File m20 =new File("sample-inputs/maze20.txt");
-		TextFileReader t = new TextFileReader(m4);
-		TextFileReader t1 = new TextFileReader(m6);
-		TextFileReader t2 = new TextFileReader(m8);
-		TextFileReader t3 = new TextFileReader(m10);
-		TextFileReader t4 = new TextFileReader(m20);
+//		TextFileReader t = new TextFileReader(m4);
+//		TextFileReader t = new TextFileReader(m6);
+//		TextFileReader t = new TextFileReader(m8);
+		TextFileReader t = new TextFileReader(m10);
+//		TextFileReader t = new TextFileReader(m20);
 
 		
 		ArrayList<String> gridList= t.copyToList();
@@ -30,13 +30,13 @@ public class FileTester {
 		dfsTest.replaceCellGrid(c);
 		dfsTest.replaceStartAndFinishCell(c);
 		ArrayList<Cell> dfs=dfsTest.solveDFS();
-		
+
 		Maze bfsTest = new Maze(t.getXSize());
 		bfsTest.replaceCellGrid(c);
 		bfsTest.replaceStartAndFinishCell(c);
-		ArrayList<ArrayList<Cell>> traversal = bfsTest.solveBFS();
-		ArrayList<Cell> bfs = traversal.get(0);
-		ArrayList<Cell> shortestPath = traversal.get(1);
+		ArrayList<ArrayList<Cell>> traversalBFS = bfsTest.solveBFS();
+		ArrayList<Cell> bfs = traversalBFS.get(0);
+		ArrayList<Cell> bfsShortestPath = traversalBFS.get(1);
 
 		ASCIIGrid dfsTestASCII = new ASCIIGrid(dfsTest, t.listToASCIIGrid());
 		ASCIIGrid bfsTestASCII = new ASCIIGrid(bfsTest, t.listToASCIIGrid());
@@ -49,8 +49,12 @@ public class FileTester {
 		System.out.println("Solved BFS: ");
 		m.printMaze(gridSolvedBFS);
 //		
-		String[][] gridSolvedShortest = bfsTestASCII.updateASCIIGridWithShortestPath(shortestPath);
+		String[][] gridSolvedShortest = bfsTestASCII.updateASCIIGridWithShortestPath(bfsShortestPath);
 		System.out.println("Solved ShortestPath: ");
 		m.printMaze(gridSolvedShortest);
+		
+		//Professor Potika's Sample Output in guidelines:
+		t.arrayToTxt(dfsTestASCII, bfsTestASCII);
+		
 	}
 }
--- CS146F19/Project3/src/cs146F19/zhu/project3/TextFileReader.java
+++ CS146F19/Project3/src/cs146F19/zhu/project3/TextFileReader.java
@@ -1,8 +1,10 @@
 package cs146F19.zhu.project3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 //Takes in a file and ultimately converts to a cell[][] that we can use to make a maze
@@ -14,143 +16,206 @@ public class TextFileReader {
 	private ArrayList<String> dataArray;
 	private int xSize;
 	private int ySize;
-	
-	//Constructor
-	public TextFileReader(File f){
+
+	// Constructor
+	public TextFileReader(File f) {
 		this.f = f;
 	}
-	
-	//Copies first line and grabs the dimensions of maze, then copies the rest into an ArrayList
+
+	// Copies first line and grabs the dimensions of maze, then copies the rest
+	// into an ArrayList
 	public ArrayList<String> copyToList() throws Exception {
-		//initialize vars
-		xSize=-1;
-		ySize=-1;
+		// initialize vars
+		xSize = -1;
+		ySize = -1;
 		dataArray = new ArrayList<String>();
 		String s;
 
 		FileReader fr = new FileReader(f);
 		BufferedReader br = new BufferedReader(fr);
-		String firstLine = br.readLine(); 
-//		System.out.println("FirstLine: " + firstLine);
-		
-		//Grab dimensions
+		String firstLine = br.readLine();
+		// System.out.println("FirstLine: " + firstLine);
+
+		// Grab dimensions
 		String[] gridSize = firstLine.split(" ");
-//		System.out.println("Size: "+gridSize.length);
-		for(int row=0; row<gridSize.length; row++){
-			if(xSize==-1){
-				xSize=Integer.parseInt(gridSize[row]);
-			}
-			else if(ySize==-1){
-				ySize=Integer.parseInt(gridSize[row]);
+		// System.out.println("Size: "+gridSize.length);
+		for (int row = 0; row < gridSize.length; row++) {
+			if (xSize == -1) {
+				xSize = Integer.parseInt(gridSize[row]);
+			} else if (ySize == -1) {
+				ySize = Integer.parseInt(gridSize[row]);
 			}
 		}
-		
-		//Copy all other lines into ArrayList
+
+		// Copy all other lines into ArrayList
 		while ((s = br.readLine()) != null) {
 			dataArray.add(s);
 		}
-		
+
 		fr.close();
 		br.close();
-		
+
 		return dataArray;
 	}
-	
-	//Takes the ArrayList of lines and converts to a String[][] ascii grid
-	public String[][] listToASCIIGrid(){
-		//initialize
-		ASCIIGrid = new String[(xSize*2)+1][(ySize*2)+1];
-		
-		//Double for loop to traverse ArrayList then every character in each string
-		for(int row=0; row<dataArray.size(); row++){
-			String arrayElement =dataArray.get(row);
+
+	// Takes the ArrayList of lines and converts to a String[][] ascii grid
+	public String[][] listToASCIIGrid() {
+		// initialize
+		ASCIIGrid = new String[(xSize * 2) + 1][(ySize * 2) + 1];
+		System.out.println("x: "+ ((xSize * 2) + 1) + ", y: "+ ((ySize *2)+1));
+
+		// Double for loop to traverse ArrayList then every character in each
+		// string
+		for (int row = 0; row < (xSize*2)+1; row++) {
+			String arrayElement = dataArray.get(row);
 			String[] splitToSingleCharacters = arrayElement.split("");
-			for(int col=0; col<splitToSingleCharacters.length; col++){
+			for (int col = 0; col < splitToSingleCharacters.length; col++) {
 				ASCIIGrid[row][col] = splitToSingleCharacters[col];
 			}
 		}
+		
 		return ASCIIGrid;
 	}
-	
-	//TO DO:
-	//Converts the String[][] ascii grid to a cell grid
-	public Cell[][] ASCIIGridToCellGrid(){
+
+	// Converts the String[][] ascii grid to a cell grid
+	public Cell[][] ASCIIGridToCellGrid() {
 		cellGrid = new Cell[xSize][ySize];
-		
-		for(int x=0; x<xSize; x++){
-			for(int y=0; y<ySize; y++){
-				cellGrid[x][y] = new Cell(x,y);
+
+		for (int x = 0; x < xSize; x++) {
+			for (int y = 0; y < ySize; y++) {
+				cellGrid[x][y] = new Cell(x, y);
 			}
 		}
-		
-		for(int x=0; x<xSize; x++){
-			for(int y=0; y<ySize; y++){
-				int xASCII = (x*2)+1;
-				int yASCII = (y*2)+1;
-//				System.out.println(xASCII + ", "+yASCII);
-				if(cellGrid[x][y] == cellGrid[0][0]){ //Special case: first cell
-					if(!ASCIIGrid[xASCII][yASCII+1].equals("|")){ //If no wall to the right, then add connection
-						cellGrid[x][y].addEdge(cellGrid[x][y+1]);
+
+		for (int x = 0; x < xSize; x++) {
+			for (int y = 0; y < ySize; y++) {
+				int xASCII = (x * 2) + 1;
+				int yASCII = (y * 2) + 1;
+				//Special case: first cell
+				if (cellGrid[x][y] == cellGrid[0][0]) { 
+					//If no wall to the right, then add connection
+					if (!ASCIIGrid[xASCII][yASCII + 1].equals("|")) { 
+						cellGrid[x][y].addEdge(cellGrid[x][y + 1]);
 					}
-					if(!ASCIIGrid[xASCII+1][yASCII].equals("-")){ //If no wall below
-						cellGrid[x][y].addEdge(cellGrid[x+1][y]);
+					//If no wall below
+					if (!ASCIIGrid[xASCII + 1][yASCII].equals("-")) { 
+						cellGrid[x][y].addEdge(cellGrid[x + 1][y]);
 					}
-				}
-				else if(cellGrid[x][y] == cellGrid[xSize-1][ySize-1]){ //Special case: last cell
-					if(!ASCIIGrid[xASCII][yASCII-1].equals("|")){ //If no wall to left
-						cellGrid[x][y].addEdge(cellGrid[x][y-1]);
+					//Special case: last cell
+				} else if (cellGrid[x][y] == cellGrid[xSize - 1][ySize - 1]) {
+					//If no wall to left
+					if (!ASCIIGrid[xASCII][yASCII - 1].equals("|")) {
+						cellGrid[x][y].addEdge(cellGrid[x][y - 1]);
 					}
-					if(!ASCIIGrid[xASCII-1][yASCII].equals("-")){ //If no wall above
-						cellGrid[x][y].addEdge(cellGrid[x-1][y]);
+					//If no wall above
+					if (!ASCIIGrid[xASCII - 1][yASCII].equals("-")) {
+						cellGrid[x][y].addEdge(cellGrid[x - 1][y]);
 					}
-				}
-				else{ //Base case
-					if((x+1)<xSize){ //Within upper row dimension bounds
-						if(!ASCIIGrid[xASCII+1][yASCII].equals("-")){ //If no wall below
-							cellGrid[x][y].addEdge(cellGrid[x+1][y]);
+				} else { // Base case
+					if ((x + 1) < xSize) { // Within upper row dimension bounds
+						//If no wall below
+						if (!ASCIIGrid[xASCII + 1][yASCII].equals("-")) { 
+							cellGrid[x][y].addEdge(cellGrid[x + 1][y]);
 						}
 					}
-					if((x-1)>0){ //Within lower row dimension bounds
-						if(!ASCIIGrid[xASCII-1][yASCII].equals("-")){ //If no wall above
-							cellGrid[x][y].addEdge(cellGrid[x-1][y]);
+					if ((x - 1) > 0) { // Within lower row dimension bounds
+						//If no wall above
+						if (!ASCIIGrid[xASCII - 1][yASCII].equals("-")) {
+							cellGrid[x][y].addEdge(cellGrid[x - 1][y]);
 						}
 					}
-					if((y-1)>0){ //Within lower col dimension bounds
-						if(!ASCIIGrid[xASCII][yASCII-1].equals("|")){ //If no wall to left
-							cellGrid[x][y].addEdge(cellGrid[x][y-1]);
+					if ((y - 1) > 0) { // Within lower col dimension bounds
+						//If no wall to left
+						if (!ASCIIGrid[xASCII][yASCII - 1].equals("|")) {
+							cellGrid[x][y].addEdge(cellGrid[x][y - 1]);
 						}
 					}
-					if((y+1)<ySize){ //Within upper col dimension bounds
-						if(!ASCIIGrid[xASCII][yASCII+1].equals("|")){ //If no wall to the right, then add connection
-							cellGrid[x][y].addEdge(cellGrid[x][y+1]);
+					if ((y + 1) < ySize) { // Within upper col dimension bounds
+						//If no wall to right
+						if (!ASCIIGrid[xASCII][yASCII + 1].equals("|")) {
+							cellGrid[x][y].addEdge(cellGrid[x][y + 1]);
 						}
 					}
 				}
 			}
 		}
-		
-		
+
 		return cellGrid;
 	}
-	//And then from here, we can probably just create a new maze, and manually change the 
-	//private variables (cell[][], vertices, all the other data needed) within the maze 
-	//and run all the algorithms needed to solve the maze.
-	
-	//BFS and DFS require connections for each cell, starting point and finishing point
-	
-	
-	public int getXSize(){
+
+	// Copy each line of the String[][] to a txt file
+	public void arrayToTxt(ASCIIGrid dfs, ASCIIGrid bfs) { 
+		try {
+			FileWriter fw = new FileWriter("text-file-output/output.txt");
+			BufferedWriter bw = new BufferedWriter(fw);
+			String[][] dfsGrid = dfs.getTraversalPathASCII();
+			String[][] bfsGrid = bfs.getTraversalPathASCII();
+			String[][] shortestGrid = bfs.getShortestPathASCII();
+			bw.write("DFS:");
+			bw.newLine();
+			for (int i = 0; i < dfsGrid.length; i++) {
+				for (int j = 0; j < dfsGrid[i].length; j++) {
+					bw.write(dfsGrid[i][j]);
+				}
+				bw.newLine();
+			}
+			bw.newLine();
+			for (int i = 0; i < shortestGrid.length; i++) {
+				for (int j = 0; j < shortestGrid[i].length; j++) {
+					bw.write(shortestGrid[i][j]);
+				}
+				bw.newLine();
+			}
+			bw.write("Path: "  );
+			bw.newLine();
+			bw.write("Length of Path: "  );
+			bw.newLine();
+			bw.write("VisitedCells: "  );
+			bw.newLine();
+			bw.newLine();
+			
+			bw.write("BFS:");
+			bw.newLine();
+			for (int i = 0; i < bfsGrid.length; i++) {
+				for (int j = 0; j < bfsGrid[i].length; j++) {
+					bw.write(bfsGrid[i][j]);
+				}
+				bw.newLine();
+			}
+			bw.newLine();
+			for (int i = 0; i < shortestGrid.length; i++) {
+				for (int j = 0; j < shortestGrid[i].length; j++) {
+					bw.write(shortestGrid[i][j]);
+				}
+				bw.newLine();
+			}
+			bw.write("Path: "  );
+			bw.newLine();
+			bw.write("Length of Path: "  );
+			bw.newLine();
+			bw.write("VisitedCells: "  );
+			bw.newLine();
+			
+			bw.close();
+			fw.close();
+		} catch (Exception e) {
+			System.out.println("Failed Output");
+		}
+	}
+
+	public int getXSize() {
 		return this.xSize;
 	}
-	public int getYSize(){
+
+	public int getYSize() {
 		return this.ySize;
 	}
-	
-	public String[][] getASCIIGrid(){
+
+	public String[][] getASCIIGrid() {
 		return this.ASCIIGrid;
 	}
-	
-	public Cell[][] getCellGrid(){
+
+	public Cell[][] getCellGrid() {
 		return this.cellGrid;
 	}
 }
